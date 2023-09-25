002b072022e89109723cc14bffb8b6eef576a2fd
JeongHanSeul <seul5106@gmail.com>
2023-09-25 17:22:28 +0900

    기본 메인 페이지 및 404 처리

---
+++ client/src/pages/MainPage.tsx
@@ -0,0 +1,9 @@
+import { FC } from "react";
+
+interface MainProps {}
+
+const MainPage: FC<MainProps> = () => {
+  return <div className="">12312412</div>;
+};
+
+export default MainPage;
---
+++ client/src/pages/NotFoundPage.tsx
@@ -0,0 +1,23 @@
+import { FC } from "react";
+import { useNavigate } from "react-router-dom";
+
+interface NotFoundPageProps {}
+
+export const NotFoundPage: FC<NotFoundPageProps> = () => {
+  const router = useNavigate();
+  return (
+    <div className="w-full h-screen flex flex-col items-center justify-center">
+      <div className="w-full text-center text-h1 font-semibold">
+        페이지를 찾을 수 없습니다.
+      </div>
+      <button
+        onClick={() => {
+          router("/");
+        }}
+        className=" rounded-[10px] text-b6 py-3 mt-[40px] w-full max-w-[300px] bg-grayscale-800 text-grayscale-white font-semibold"
+      >
+        홈으로
+      </button>
+    </div>
+  );
+};
