a2b1d66910775c40ef1f62b6cb0b35df501b16c1
JeongHanSeul <seul5106@gmail.com>
2023-09-26 16:00:26 +0900

    라우터에 lazy 추가

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,19 +1,23 @@
-import { FC, Suspense } from "react";
+import { FC, Suspense, lazy } from "react";
 import { Routes, Route, BrowserRouter } from "react-router-dom";
-import { NotFoundPage } from "./NotFoundPage";
-import MainPage from "./MainPage";
 import { LoadingScreen } from "../component/Loading/LoadingScreen";
+import { Header } from "../component/Layout/Header";
 
 interface PagesProps {}
 
 export const Pages: FC<PagesProps> = () => {
+  const MainP = lazy(() => import("./MainPage"));
+  const NotFoundP = lazy(() => import("./NotFoundPage"));
+
   return (
     <BrowserRouter>
-      <Suspense fallback={<LoadingScreen />} />
-      <Routes>
-        <Route path="/" element={<MainPage />} />
-        <Route path="*" element={<NotFoundPage />} />
-      </Routes>
+      <Header />
+      <Suspense fallback={<LoadingScreen />}>
+        <Routes>
+          <Route path="/" element={<MainP />} />
+          <Route path="*" element={<NotFoundP />} />
+        </Routes>
+      </Suspense>
     </BrowserRouter>
   );
 };
